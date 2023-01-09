76bb2ae579d807b29a8885af67cdbd88c9c30779
Elisa Honorato <elisahonoratolira@uc.cl>
2023-01-09 08:28:00 -0300

    md

---
+++ frontend/build/precache-manifest.842b7cd9d7394c8ce31ded75e8f8d865.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e4396ca2befccb0835d",
+    "url": "/liho/static/css/main.3f80375c.chunk.css"
+  },
+  {
+    "revision": "8e4396ca2befccb0835d",
+    "url": "/liho/static/js/main.8e4396ca.chunk.js"
+  },
+  {
+    "revision": "e2723de9d6e9385cbe1e",
+    "url": "/liho/static/js/1.e2723de9.chunk.js"
+  },
+  {
+    "revision": "68d16ae22ea9b18faae5",
+    "url": "/liho/static/css/2.a90f5291.chunk.css"
+  },
+  {
+    "revision": "68d16ae22ea9b18faae5",
+    "url": "/liho/static/js/2.68d16ae2.chunk.js"
+  },
+  {
+    "revision": "7e1d87d720d615d957b1",
+    "url": "/liho/static/js/runtime~main.7e1d87d7.js"
+  },
+  {
+    "revision": "e8fbf654c89d2c7b366c82f333858ed3",
+    "url": "/liho/static/media/logo.e8fbf654.svg"
+  },
+  {
+    "revision": "dd3dbb5b981d80ce6a97bddf9fce6e71",
+    "url": "/liho/index.html"
+  }
+];
