d3df70eef5e1d2d75dfa3875a9861f0d63367b07
Elisa Honorato <elisahonoratolira@uc.cl>
2023-05-12 19:19:04 -0400

    feat explanation of data

--- frontend/src/components/Pages/Tutorial/MuiTable.js
+++ frontend/src/components/Pages/Tutorial/MuiTable.js
@@ -1,34 +1,63 @@
 import { useState } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Tooltip } from '@mui/material';
 
-
+const columns = [
+  { id: 'id', label: 'Nombre Muestra', tooltip: 'Nombre de la muestra del tipo String'},
+  { id: 'variable1', label: 'Variable 1', tooltip: 'Numero del 0 al 1' },
+  { id: 'variable2', label: 'Variable 2', tooltip: 'Numero del 0 al 1' },
+  { id: 'variable3', label: 'Variable 3', tooltip: 'Numero del 0 al 1' },
+  { id: 'variable4', label: 'Variable 4', tooltip: 'Numero del 0 al 1' },
+  { id: 'variable5', label: 'Variable 5', tooltip: 'Numero del 0 al 1' },
+  { id: 'variable6', label: 'Variable 6', tooltip: 'Numero del 0 al 1' },
+];
 
 export default function MuiTable() {
-  // Here you can define your data state
-  const [data, setData] = useState([
-    { id: "Muestra 1", name: 'John Doe', age: 30 },
-    { id: "Muestra 2", name: 'Jane Smith', age: 25 },
-    { id: "Muestra 3", name: 'Bob Johnson', age: 40 },
-  ]);
+  // Generate random data for each row of the table
+  const data = [...Array(5)].map((_, i) => ({
+    id: `Muestra ${i + 1}`,
+    variable1: Math.random(),
+    variable2: Math.random(),
+    variable3: Math.random(),
+    variable4: Math.random(),
+    variable5: Math.random(),
+    variable6: Math.random(),
+  }));
+
+  const [tooltipOpen, setTooltipOpen] = useState(null);
+
+  const handleTooltipOpen = (event) => {
+    setTooltipOpen(event.currentTarget);
+  };
+
+  const handleTooltipClose = () => {
+    setTooltipOpen(null);
+  };
 
   return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
           <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Age</TableCell>
-            <TableCell>Actions</TableCell>
+            {columns.map((column) => (
+              <TableCell key={column.id}>
+                <Tooltip title={column.tooltip}>
+                  <span onMouseEnter={handleTooltipOpen} onMouseLeave={handleTooltipClose}>
+                    {column.label}
+                  </span>
+                </Tooltip>
+              </TableCell>
+            ))}
           </TableRow>
         </TableHead>
         <TableBody>
           {/* Map through the data to create a row for each object */}
           {data.map((row) => (
             <TableRow key={row.id}>
-              <TableCell>{row.id}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.age}</TableCell>
+              {columns.map((column) => (
+                <TableCell key={column.id}>
+                  {column.id === 'id' ? row[column.id] : row[column.id].toFixed(3)}
+                </TableCell>
+              ))}
               <TableCell>
                 {/* Here you can add your actions buttons */}
               </TableCell>
--- frontend/src/components/Pages/Tutorial/Tutorial.js
+++ frontend/src/components/Pages/Tutorial/Tutorial.js
@@ -1,11 +1,28 @@
-import { Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { Typography, Container, Box, Button } from '@mui/material';
 import MuiTable from './MuiTable';
 
-export default function Home() {
+export default function Tutorial() {
   return (
-    <div>
-      <Typography variant="body1">Ejemplo de Tabla</Typography>
-      <MuiTable />
-    </div>
+    <Container maxWidth="md" sx={{ mt: 4 }}>
+      <Box sx={{ mb: 4 }}>
+        <Typography variant="h4" component="h1" align="center" gutterBottom>
+          Ejemplo de Tabla
+        </Typography>
+        <Typography variant="body1" align="justify" gutterBottom>
+          Esta es una tabla de ejemplo con datos generados aleatoriamente. Cada fila representa una muestra, y las variables son valores numéricos aleatorios entre 0 y 1 con una precisión de 3 decimales. Puedes personalizar esta tabla y sus datos según tus necesidades.
+        </Typography>
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 4 }}>
+        <MuiTable />
+      </Box>
+      <Box sx={{ mb: 4, display: 'grid', gap: 20, justifyContent: 'center', alignItems: 'center' }}>
+        <Button component={Link} to="/" variant="contained">
+            Generar Gráfico
+        </Button>
+    </Box>
+
+
+    </Container>
   );
 }
