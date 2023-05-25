dc3ff5e2407871bb70f10249a568d863582d5bd9
Elisa Honorato <elisahonoratolira@uc.cl>
2023-05-25 16:58:56 -0400

    First glb uploaded

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,4 @@
-from django.http import FileResponse, HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import MultiPartParser
 from rest_framework.views import APIView
 
@@ -6,33 +6,37 @@ from .models import *
 from .serializers import *
 
 
-# Create your views here.
 class PruebaView(APIView):
     parser_classes = [MultiPartParser]
 
     def post(self, request):
-        uploaded_file = request.FILES["file"]
+        uploaded_file = request.FILES.get("file")
+        if not uploaded_file:
+            return HttpResponse("Error: No file uploaded", status=400)
+
         if uploaded_file.content_type == "text/csv":
-            file = File.objects.create(url=uploaded_file)
+            user_filename = request.data.get("userFilename")
+            if user_filename == "undefined":
+                user_filename = "file"
+            file = File.objects.create(url=uploaded_file, title=user_filename)
             gltf = GLTFFile.objects.create(file=file)
-            n_samples = request.POST.get("n_samples")
-            n_columns = request.POST.get("n_columns")
-            print(request.POST, n_samples, n_columns)
+            n_samples = request.data.get("n_samples")
+            n_columns = request.data.get("n_columns")
+
             if n_samples == "null":
                 n_samples = None
             if n_columns == "null":
                 n_columns = None
-            gltf.generate_gltf(n_samples, n_columns)
+
+            gltf.generate_gltf(n_samples, n_columns, user_filename)
+
             if gltf.exists:
                 return JsonResponse(gltf.dict, status=200)
-            print("Error al generar el archivo")
-            return HttpResponse({"Error al generar el archivo"}, status=400)
-
-        elif uploaded_file.content_type != "text/csv":
-            return HttpResponse({"Archivo con el formato incorrecto"}, status=400)
+            else:
+                return HttpResponse("Error al generar el archivo", status=400)
 
         else:
-            return HttpResponse({"Error desconocido"}, status=400)
+            return HttpResponse("Archivo con formato incorrecto", status=400)
 
     def get(self, request):
-        return HttpResponse({"Metodo no permitido"}, status=405)
+        return HttpResponse("Método no permitido", status=405)
