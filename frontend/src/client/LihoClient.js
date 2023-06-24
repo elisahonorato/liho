03bb3e06d772a56c6b62ee2bab64d0ab28ec9ea4
Elisa <elisahonoratolira@uc.cl>
2023-06-24 19:25:16 -0400

    render-probando

--- frontend/src/client/LihoClient.js
+++ frontend/src/client/LihoClient.js
@@ -1,22 +1,19 @@
 import React from "react";
 import { ApiProvider } from "react-rest-api";
 
-const LihoClient = ({children}) => {
-    const baseUrl = "http://localhost:8000/probando/";
+const LihoClient = ({ children }) => {
+    const baseUrl = "https://your-render-url/probando/";
     const config = {
-        headers: {
-            // All api calls will take this Content-Type Header
-            "Content-Type": "multipart/form-data",
-        }
-    }
+      headers: {
+        // All API calls will take this Content-Type Header
+        "Content-Type": "multipart/form-data",
+      },
+    };
     return (
-        <ApiProvider
-            url={baseUrl}
-            config={config}
-        >
-            {children}
-        </ApiProvider>
+      <ApiProvider url={baseUrl} config={config}>
+        {children}
+      </ApiProvider>
     );
-}
-
-export default LihoClient
+  };
+  
+  export default LihoClient;
