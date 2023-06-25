0e408fafd4acd48959468396cabbc7a9ec68bcff
Elisa <elisahonoratolira@uc.cl>
2023-06-24 20:00:54 -0400

    render database

--- backend/liho/settings.py
+++ backend/liho/settings.py
@@ -81,19 +81,20 @@ WSGI_APPLICATION = "liho.wsgi.application"
 
 
 # Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
-
-# Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgres://liho_postgresql_user:AhHOYDKIg3WbplWnDS579h9civNoKQTC@dpg-cibnhud9aq03rjgjtn40-a.oregon-postgres.render.com/liho_postgresql',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+if 'RENDER_DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        default=os.environ.get('RENDER_DATABASE_URL'),
         conn_max_age=600
     )
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
