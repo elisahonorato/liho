b42ffcef3541feacab4514c377a7a7797c3595e5
Elisa <elisahonoratolira@uc.cl>
2023-06-24 23:03:54 -0400

    render

--- frontend/src/client/LihoClient.js
+++ frontend/src/client/LihoClient.js
@@ -2,11 +2,13 @@ import React from "react";
 import { ApiProvider } from "react-rest-api";
 
 const LihoClient = ({ children }) => {
-    const baseUrl = "https://liho.onrender.com/probando";
+    const baseUrl = "http://localhost:8000/probando";
+    // const baseUrl = "https://liho.onrender.com/probando";
     const config = {
       headers: {
         // All API calls will take this Content-Type Header
         "Content-Type": "multipart/form-data",
+        
       },
     };
     return (
