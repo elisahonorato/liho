227f6d5c2536100117f4fbf59a4f5f2775f0e3a2
Elisa <elisahonoratolira@uc.cl>
2023-06-24 21:42:07 -0400

    sds

--- backend/liho/settings.py
+++ backend/liho/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -46,7 +47,6 @@ INSTALLED_APPS = [
     "api",
     "rest_framework",
     "corsheaders",
-    'render.apps.RenderConfig',
 ]
 
 MIDDLEWARE = [
