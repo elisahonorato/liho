9470d67614b51855d9ac4e47cdbbfa071619490f
Elisa <elisahonoratolira@uc.cl>
2023-07-05 22:02:16 -0400

    build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,33 +4,34 @@ import { ThemeProvider } from '@mui/material/styles';
 
 
 import theme from './components/Theme/Theme';
-import { Routes, Route} from "react-router-dom";
 import Home from './components/Pages/Home'
 import Header from './components/Header/Header';
 import Tutorial from './components/Pages/Tutorial';
 import About from './components/Pages/About/About';
 import { Container, Typography} from '@mui/material';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
 function App() {
   return (
     <LihoClient>
       <ThemeProvider theme={theme}>
-        <Header />
-          <Container maxWidth="xl" style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/liho" element={<Home />} />
-            <Route path="/about" element={<About/>} />
-            <Route path="/tutorial" element={<Tutorial />} />
-          </Routes>
-          <footer style={{padding: "20px", marginTop: "auto" }}>
-            <Typography variant="body1" align="center">
-                © 2023 Liho. Todos los derechos reservados.
-              </Typography>
-          </footer>
-          </Container>
+        <Router>
+          <Header />
+            <Container maxWidth="xl" style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/liho" element={<Home />} />
+              <Route path="/about" element={<About/>} />
+              <Route path="/tutorial" element={<Tutorial />} />
+            </Routes>
+            <footer style={{padding: "20px", marginTop: "auto" }}>
+              <Typography variant="body1" align="center">
+                  © 2023 Liho. Todos los derechos reservados.
+                </Typography>
+            </footer>
+            </Container>
+        </Router>
       </ThemeProvider>
     </LihoClient>
   );
